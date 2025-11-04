f15043c2762b48adc76025dfde5a2a01425beb7b
Manoel Machado <m.machado49@hotmail.com>
2025-11-03 21:29:34 -0300

    criação de pastas para melhorar a organaização
    criamos as pastas Contaainer e Heading, colocamos os componentes nas respectivas pastas e alteramos os nomes dos componentes para uma melhor organização

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./Heading.module.css";
+import styles from "./styles.module.css";
 
 type HeadingProps = {
   children: React.ReactNode; /*ReactNode faz com que aceite tudo que o React considere como children*/
