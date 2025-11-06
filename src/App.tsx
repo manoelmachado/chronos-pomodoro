2d7160f50561badf2de7715f2a7a9e17bce5c9e8
Manoel Machado <machadodevs@gmail.com>
2025-11-06 16:51:13 -0300

    formulário da página principal
    nesse commit, criamos o formulário base da página principal, ainda vamos modificar o CSS e criar corretamente os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import "./styles/global.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
-import { CountDown } from "./components/CountDown"
-
+import { CountDown } from "./components/CountDown";
 
 function App() {
   return (
@@ -18,7 +17,29 @@ function App() {
       </Container>
 
       <Container>
-        <CountDown/>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <label htmlFor="input">task</label>
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
