b97ba4d15945dc21e43f58f5d7a5019478818972
Manoel Machado <machadodevs@gmail.com>
2025-11-11 16:06:42 -0300

    condicionais no componente
    adicionei condições no componente para que se existir o labelText, ele vai aparecer, caso não, não vai aparecer

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <Container>
         <form className="form" action="">
           <div className="formRow">
-            <DefaultInput id="meuInput" type="text" />
+            <DefaultInput id="meuInput" type="text" labelText="task"/>
           </div>
 
           <div className="formRow">
