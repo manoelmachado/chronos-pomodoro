48c1a01712ac36e141037ce4a0142469849e15e2
Manoel Machado <machadodevs@gmail.com>
2025-11-18 09:44:41 -0300

    capturando evento de clique
    capturamos o evento de clique para alternar entre o tema dark e light e também utilizamos o evento com preventDefault para que ele não siga para a página do link informado

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,49 @@
 import styles from "./styles.module.css";
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useState } from "react";
+
+type AvaliableThemes = 'dark' | 'light'
 
 export function Menu() {
-  /*desestruturação do props.children do js*/
+  const [theme, setTheme] = useState<AvaliableThemes>('dark')
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); //não segue o link, não segue a ação padrão de ir para o link
+  }
+  
   return (
     <nav className={`${styles.menu}`}>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Histórico"
+        title="Histórico"
+      >
         <HistoryIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configurações"
+        title="Configurações"
+      >
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Alterar Tema"
+        title="Alterar Tema"
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
